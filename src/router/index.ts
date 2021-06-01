3cc053f252b7686d94b16f0dad26c1f1685c31fe
chhaya <chhptl8@gmail.com>
2021-06-01 13:32:18 +0530

    Rv and  its Modal added

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,23 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import Home from '../views/Home.vue'
+import Home from '../views/Home.vue';
+import Rv from '../views/Rv.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
+    path: '/h',
     redirect: '/home'
   },
   {
     path: '/home',
     name: 'Home',
     component: Home
-  }
+  },
+  {
+    path: "/rv",
+    name: "Rv",
+    component: Rv,
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/Rv.vue
@@ -0,0 +1,154 @@
+<template>
+  <ion-page>
+    <!-- <ion-header :translucent="true"> </ion-header> -->
+    <ion-toolbar class="ion-no-border">
+      <ion-item class="ion-no-padding ion-no-border">
+        <ion-avatar slot="end">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ492AnNAAPJR3tuNh9iojWzyaVnyeQ3mApIw&usqp=CAU"
+          />
+        </ion-avatar>
+      </ion-item>
+    </ion-toolbar>
+    <ion-content class="ion-padding">
+      <ion-toolbar>
+        <ion-title>
+          <h3 class="title">Your Rv's</h3>
+        </ion-title>
+      </ion-toolbar>
+
+      <ion-list
+        class="ion-padding-top"
+        v-for="Rv in totalRvs"
+        v-bind:key="Rv._id"
+      >
+        <ion-item class="ion-no-padding ion-margin-bottom" href="/room">
+          <ion-avatar slot="start">
+            <img
+              src="https://st.depositphotos.com/1203257/1763/i/950/depositphotos_17637335-stock-photo-yellowstone-rv-trip.jpg"
+            />
+          </ion-avatar>
+
+          <ion-label>{{ Rv.name }}</ion-label>
+          <ion-badge color="primary" class="ion-margin-end">10+</ion-badge>
+          <div>
+            <div class="circle">
+              <ion-text color="light">
+                <ion-icon src="/assets/icon/share.svg"></ion-icon>
+              </ion-text>
+            </div>
+            <div class="small">
+              Share
+            </div>
+          </div>
+        </ion-item>
+      </ion-list>
+      <ion-fab
+        style="margin-bottom:60px"
+        @click="openAddRv"
+        horizontal="end"
+        vertical="bottom"
+        slot="fixed"
+      >
+        <ion-fab-button color="primary" style="margin:auto">
+          <ion-icon :icon="add"></ion-icon>
+        </ion-fab-button>
+        <h6>Add an RV</h6>
+      </ion-fab>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import {
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonHeader,
+  IonContent,
+  IonIcon,
+  IonAvatar,
+  IonButton,
+  IonModal,
+  IonBadge,
+  IonFab,
+  IonFabButton,
+  modalController,
+} from "@ionic/vue";
+import { add } from "ionicons/icons";
+import RvAddModal from "./RvAddModal.vue";
+import axios from "axios";
+export default {
+  name: "Rv",
+  component: {
+    IonPage,
+    IonToolbar,
+    IonTitle,
+    IonHeader,
+    IonContent,
+    IonIcon,
+    IonAvatar,
+    IonButton,
+    IonModal,
+    IonBadge,
+    IonFab,
+    IonFabButton,
+  },
+  setup() {
+    return {
+      add,
+    };
+  },
+
+  data() {
+    return {
+      totalRvs: [],
+    };
+  },
+
+  created() {
+    this.listOfRvs();
+  },
+  methods: {
+    async listOfRvs() {
+      const options = {
+        method: "POST",
+        url: "http://localhost:3000/Rv/search",
+        headers: { "Content-Type": "application/json" },
+        data: { creator: "507f1f77bcf86cd799439014", page: 4 },
+      };
+      const data = await axios.request(options);
+
+      this.totalRvs = data.data.data;
+      console.log(this.totalRvs);
+    },
+
+    async openAddRv() {
+      const modal = await modalController.create({
+        component: RvAddModal,
+        cssClass: "my-custom-class",
+        swipeToClose: true,
+        componentProps: {
+          title: "New Title",
+        },
+      });
+      return modal.present();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.circle {
+  width: 22px;
+  height: 22px;
+  background: #ffab01;
+  font-size: 12px;
+  border-radius: 50%;
+  padding: 4px;
+  margin: 0 auto;
+}
+.small {
+  font-size: 12px;
+}
+</style>
